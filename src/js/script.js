c4c601032058524d3ec70a844469736617734aa8
@Abdull <muhammad065932@smk.belajar.id>
2023-12-08 14:58:48 +0700

    second commit

--- src/js/script.js
+++ src/js/script.js
@@ -1,56 +1,50 @@
-const navlinkEls = document.querySelectorAll('.nav-link')
-const sectionEls = document.querySelectorAll('.section')
+const navlinkEls = document.querySelectorAll(".nav-link");
+const sectionEls = document.querySelectorAll(".section");
 
-
-let currentSection = 'home'
-window.addEventListener('scroll', () => {
-    sectionEls.forEach(sectionEl => {
-        if (window.scrollY >= sectionEl.offsetTop) {
-            currentSection = sectionEl.id
-        }
-    });
-    navlinkEls.forEach(navlinkEl => {
-        if (navlinkEl.href.includes(currentSection)) {
-            navlinkEl.classList.add('active')
-        } else {
-            navlinkEl.classList.remove('active')
-        }
-    })
-})
+let currentSection = "home";
+window.addEventListener("scroll", () => {
+  sectionEls.forEach((sectionEl) => {
+    if (window.scrollY >= sectionEl.offsetTop) {
+      currentSection = sectionEl.id;
+    }
+  });
+  navlinkEls.forEach((navlinkEl) => {
+    if (navlinkEl.href.includes(currentSection)) {
+      navlinkEl.classList.add("active");
+    } else {
+      navlinkEl.classList.remove("active");
+    }
+  });
+});
 
 // Dynamic Menu
 
 // Fetch JSON
 async function getMenuData() {
-    const endpoint = await fetch('/db/menu.json')
-    const menuData = endpoint.json
-    console.log(menuData);
+  const endpoint = await fetch("db/menu.json");
+  const menuData = await endpoint.json();
+  console.log(menuData.data);
+  console.log(menuData.data.image);
+
+  menuData.data.forEach((element) => {
+    addMenuData(element.image, element.title);
+    console.log(element.image);
+  });
 }
 
+getMenuData()
 
+const rowParrentMenu = document.getElementById("row-menu");
+console.log(rowParrentMenu);
 
-
-
-const rowParrentMenu = document.getElementById('row-menu')
-const menuWrapper = document.createElement('div')
-menuWrapper.classList.add('col-sm-4', 'col-12')
-menuWrapper.appendChild(menuCard)
-
-const menuCard = document.createElement('div')
-menuCard.classList.add('menu-card')
-menuCard.appendChild(menuLink)
-
-const menuLink = document.createElement('a')
-menuLink.appendChild(menuIcon)
-menuLink.appendChild(menuTitle)
-
-
-let menuIcon = document.createElement('div')
-menuIcon.classList.add('menu-icon')
-menuIcon.appendChild(menuIconImg)
-
-let menuIconImg = document.createElement('img')
-menuIconImg.href = img
-
-let menuTitle = document.createElement('div')
-menuTitle.textContent = menuTitle
+const addMenuData = (img, title) => {
+  let get = ` <div class="col-sm-4 col-12">
+    <div class="menu-card">
+        <a href="">
+            <div class="menu-icon"><img src="${img}" class="w-75"></div>
+            <div class="menu-title">${title}</div>
+        </a>
+    </div>
+</div>`;
+  rowParrentMenu.innerHTML += get;
+};
