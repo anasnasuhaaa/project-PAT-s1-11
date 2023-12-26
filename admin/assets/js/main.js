199daeff40058d17206835831fe6170dc48d1e81
usmanlubis <usmanlubis11@gmail.com>
2023-12-26 22:43:47 +0700

    final

--- admin/assets/js/main.js
+++ admin/assets/js/main.js
@@ -1,21 +1,56 @@
 // add hovered class to selected list item
-let list = document.querySelectorAll(".navigation li");
+let list = document.querySelectorAll('.navigation li');
 
 function activeLink() {
-  list.forEach((item) => {
-    item.classList.remove("hovered");
-  });
-  this.classList.add("hovered");
+	list.forEach((item) => {
+		item.classList.remove('hovered');
+	});
+	this.classList.add('hovered');
 }
 
-list.forEach((item) => item.addEventListener("mouseover", activeLink));
+list.forEach((item) => item.addEventListener('mouseover', activeLink));
 
 // Menu Toggle
-let toggle = document.querySelector(".toggle");
-let navigation = document.querySelector(".navigation");
-let main = document.querySelector(".main");
+let toggle = document.querySelector('.toggle');
+let navigation = document.querySelector('.navigation');
+let main = document.querySelector('.main');
 
 toggle.onclick = function () {
-  navigation.classList.toggle("active");
-  main.classList.toggle("active");
+	navigation.classList.toggle('active');
+	main.classList.toggle('active');
+};
+
+// Daftar Guru
+async function getMenuData() {
+	const endpoint = await fetch('/project-PAT-s1-11/db/guru.json');
+	const menuData = await endpoint.json();
+	console.log(menuData.dataGuru);
+
+	menuData.dataGuru.forEach((element) => {
+		addMenuData(element.fotoProfile, element.name, element.asal);
+	});
+}
+
+getMenuData();
+
+const parrent = document.getElementById('table');
+console.log(parrent);
+
+const addMenuData = (img, name, origin) => {
+	let get = ` 
+  <tr>
+    <td width="60px">
+									<div class="imgBx">
+										<img src="${img}" alt="" />
+									</div>
+								</td>
+								<td>
+									<h4>
+										${name} <br />
+										<span>${origin}</span>
+									</h4>
+								</td>
+                  </tr>`;
+
+	parrent.innerHTML += get;
 };
--- src/js/script.js
+++ src/js/script.js
@@ -1,44 +1,44 @@
-const navlinkEls = document.querySelectorAll(".nav-link");
-const sectionEls = document.querySelectorAll(".section");
+const navlinkEls = document.querySelectorAll('.nav-link');
+const sectionEls = document.querySelectorAll('.section');
 
-let currentSection = "home";
-window.addEventListener("scroll", () => {
-    sectionEls.forEach((sectionEl) => {
-        if (window.scrollY >= sectionEl.offsetTop) {
-            currentSection = sectionEl.id;
-        }
-    });
-    navlinkEls.forEach((navlinkEl) => {
-        if (navlinkEl.href.includes(currentSection)) {
-            navlinkEl.classList.add("active");
-        } else {
-            navlinkEl.classList.remove("active");
-        }
-    });
+let currentSection = 'home';
+window.addEventListener('scroll', () => {
+	sectionEls.forEach((sectionEl) => {
+		if (window.scrollY >= sectionEl.offsetTop) {
+			currentSection = sectionEl.id;
+		}
+	});
+	navlinkEls.forEach((navlinkEl) => {
+		if (navlinkEl.href.includes(currentSection)) {
+			navlinkEl.classList.add('active');
+		} else {
+			navlinkEl.classList.remove('active');
+		}
+	});
 });
 
 // Dynamic Menu
 
 // Fetch JSON
 async function getMenuData() {
-    const endpoint = await fetch("db/menu.json");
-    const menuData = await endpoint.json();
-    console.log(menuData.data);
-    console.log(menuData.data.image);
+	const endpoint = await fetch('db/menu.json');
+	const menuData = await endpoint.json();
+	console.log(menuData.data);
+	console.log(menuData.data.image);
 
-    menuData.data.forEach((element) => {
-        addMenuData(element.image, element.title);
-        console.log(element.image);
-    });
+	menuData.data.forEach((element) => {
+		addMenuData(element.image, element.title);
+		console.log(element.image);
+	});
 }
 
-getMenuData()
+getMenuData();
 
-const rowParrentMenu = document.getElementById("row-menu");
+const rowParrentMenu = document.getElementById('row-menu');
 console.log(rowParrentMenu);
 
 const addMenuData = (img, title) => {
-    let get = ` <div class="col-sm-4 col-12">
+	let get = ` <div class="col-sm-4 col-12 d-flex justify-content-center align-items-center">
     <div class="menu-card animate__animated">
         <a href="guru.html">
             <div class="menu-icon"><img src="${img}" class="w-75"></div>
@@ -47,20 +47,20 @@ const addMenuData = (img, title) => {
     </div>
 </div>`;
 
-    rowParrentMenu.innerHTML += get;
+	rowParrentMenu.innerHTML += get;
 };
 
 fetch('db/selfdevelopment.json')
-    .then(res => res.json())
-    .then(selfDev => {
-        selfDev.forEach(element => {
-            getSelfDev(element.img, element.icon, element.title, element.description)
-        })
-    })
+	.then((res) => res.json())
+	.then((selfDev) => {
+		selfDev.forEach((element) => {
+			getSelfDev(element.img, element.icon, element.title, element.description);
+		});
+	});
 
-const selfDevRow = document.getElementById('sd-row')
+const selfDevRow = document.getElementById('sd-row');
 const getSelfDev = (img, icon, title, desc) => {
-    let selfDevCol = `
+	let selfDevCol = `
     <div class="col-12 col-sm-6 col-md-4 ">
                     <div class="sd-card bg-light rounded-3 d-flex flex-column justify-content-between">
                         <img src="${img}" class="self-development-img rounded-3" alt="">
@@ -74,20 +74,25 @@ const getSelfDev = (img, icon, title, desc) => {
                 </div>
     `;
 
-    selfDevRow.innerHTML += selfDevCol
-}
+	selfDevRow.innerHTML += selfDevCol;
+};
 
 fetch('db/inspirasi.json')
-    .then(res => res.json())
-    .then(inspiration => {
-        inspiration.forEach(element => {
-            getInspiration(element.img, element.icon, element.title, element.description)
-        })
-    })
+	.then((res) => res.json())
+	.then((inspiration) => {
+		inspiration.forEach((element) => {
+			getInspiration(
+				element.img,
+				element.icon,
+				element.title,
+				element.description
+			);
+		});
+	});
 
-const inspirationRow = document.getElementById('sd-row-inspiration')
+const inspirationRow = document.getElementById('sd-row-inspiration');
 const getInspiration = (img, icon, title, desc) => {
-    let inspirationCol = `   <div class="col-12 col-sm-6 col-md-4 ">
+	let inspirationCol = `   <div class="col-12 col-sm-6 col-md-4 ">
     <div class="sd-card bg-light rounded-3 d-flex flex-column justify-content-between">
         <img src="${img}" class="self-development-img rounded-3" alt="">
         <div class="header-card-sd d-flex justify-content-start align-items-center mb-3 mt-3">
@@ -100,20 +105,25 @@ const getInspiration = (img, icon, title, desc) => {
 </div>
 `;
 
-    inspirationRow.innerHTML += inspirationCol;
-}
+	inspirationRow.innerHTML += inspirationCol;
+};
 
 fetch('db/mengajar.json')
-    .then(res => res.json())
-    .then(mengajar => {
-        mengajar.forEach(element => {
-            getMengajar(element.img, element.icon, element.title, element.description)
-        })
-    })
+	.then((res) => res.json())
+	.then((mengajar) => {
+		mengajar.forEach((element) => {
+			getMengajar(
+				element.img,
+				element.icon,
+				element.title,
+				element.description
+			);
+		});
+	});
 
-const mengajarRow = document.getElementById('sd-row-mengajar')
+const mengajarRow = document.getElementById('sd-row-mengajar');
 const getMengajar = (img, icon, title, desc) => {
-    let mengajarCol = `   <div class="col-12 col-sm-6 col-md-4 ">
+	let mengajarCol = `   <div class="col-12 col-sm-6 col-md-4 ">
     <div class="sd-card bg-light rounded-3 d-flex flex-column justify-content-between">
         <img src="${img}" class="self-development-img rounded-3" alt="">
         <div class="header-card-sd d-flex justify-content-start align-items-center mb-3 mt-3">
@@ -126,5 +136,5 @@ const getMengajar = (img, icon, title, desc) => {
 </div>
 `;
 
-    mengajarRow.innerHTML += mengajarCol;
-}
+	mengajarRow.innerHTML += mengajarCol;
+};
